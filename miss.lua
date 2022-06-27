81fb8b03147c9b74a0a842b0f2a378900a290199
Pakie Bailey <30850366+pakieFNF@users.noreply.github.com>
2022-06-27 15:55:10 -0500

    Update miss.lua

--- miss.lua
+++ miss.lua
@@ -1,4 +1,6 @@
 function noteMiss(id, direction, noteType, isSustainNote)
+        -- im sorry psych discord mods i have 0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000.1IQ 
+        -- and im mentally stupid please dont strap me to a wall in the basement and proceed to throw knifes at me so i can be murdered
         miss = string.format('missnote%i', math.random(1, 3))
         playSound(miss, 1)
 end
